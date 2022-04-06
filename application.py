949d66111f4e51e7935d5b419448081ec85e66c1
Evilmakoto <keisuke.nttn@gmail.com>
2022-04-06 20:08:03 +0900

    first commit

---
+++ application.py
@@ -0,0 +1,44 @@
+# 筋トレツイートbot
+from config import settings
+import tweepy
+import random
+
+# 初期設定
+client = tweepy.Client(
+    consumer_key=settings.API_KEY,
+    consumer_secret=settings.API_SECRET,
+    access_token=settings.ACCESS_TOKEN,
+    access_token_secret=settings.ACCESS_TOKEN_SECRET
+)
+
+# トレーニング決定
+rand_menu = random.randrange(
+    0, len(settings.TRAINING)
+    )
+
+
+# 筋トレメニュー決定
+training_menu = settings.TRAINING[rand_menu]
+
+# ツイート内容
+if training_menu == '休み':
+    message = f'今日のトレーニングは{training_menu}です!'
+
+elif training_menu == '自転車':
+    message = f'今日は{training_menu}でトレーニングしましょう!'
+
+elif training_menu == 'プランク':
+    message = f'今日は{training_menu}を1分3セットしましょう!'
+
+else:
+    # トレーニング回数とセット数決定
+    rand_num = random.randrange(
+        0, len(settings.NUM)
+    )
+    rand_set = random.randrange(
+        1 , 3
+    )
+    message = f'今日は{training_menu}を{settings.NUM[rand_num]}回を{rand_set}セットしましょう!'
+
+# ツイート
+client.create_tweet(text=message)
---
+++ config/settings.py
@@ -0,0 +1,24 @@
+# Key Token
+API_KEY = 'NYHSJQr22fNVGaSxNmKToHe7Z'
+API_SECRET = 'wiM0iwh9Zzb0YQYLRh9iRHpXGYkm9d7neDlULSFwZMJ9FGfNx3'
+ACCESS_TOKEN = '717728778458308610-sgph8eF6aCRQIa8Vef0jQ0NHFapOfZ4'
+ACCESS_TOKEN_SECRET = 'NR2rXttBVk85y1XIfL9yzqUnl2ZjkhhBHINfg9iwtVBH6'
+
+
+# 筋トレメニュー
+TRAINING = ([
+    '休み',
+    '腕立て伏せ',
+    'スクワット',
+    '自転車',
+    'プランク',
+    '腹筋ローラー'
+])
+
+# 筋トレ回数
+NUM = ([
+    15,
+    20,
+    25,
+    30,
+])
